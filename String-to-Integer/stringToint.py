23cd662c072f40fec35fda7147e9d4b94d76657a
miranmanesh <seiranmanesh@mix.wvu.edu>
2016-10-14 11:26:06 -0700

    Add files via upload

---
+++ String-to-Integer/stringToint.py
@@ -0,0 +1,20 @@
+import re
+class Solution(object):
+    def myAtoi(self, str):
+        r = re.match("[+-]?[0-9]+", str.strip())
+        if not r:
+            return 0
+        y = int(r.group())
+        if y < -2147483648:
+            y = -2147483648 
+        if y > 2147483647:
+            y = 2147483647
+        return y
+    
+    
+    
+    
+    
+    
+s = Solution()
+print(s.myAtoi('-123'))
